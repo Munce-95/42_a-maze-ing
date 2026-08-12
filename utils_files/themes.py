72e5d0f33e895765df4c7163b08fd1c455c9ca37
Celian Gremy <ceceoverwatch@gmail.comgit>
2026-08-12 17:08:58 +0200

    COULEUR

--- utils_files/themes.py
+++ utils_files/themes.py
@@ -15,15 +15,16 @@ class Theme(NamedTuple):
     wall: RGB
     logo: RGB
     path: RGB
+    way: RGB
     name: str
 
 THEMES = [
-    Theme(RGB(42, 42, 42), RGB(85, 60, 100), RGB(200, 200, 200), "b/w"),
-    Theme(RGB(50, 5, 85), RGB(253, 0, 219), RGB(100, 30, 90), "pink"),
-    Theme(RGB(15, 23, 42), RGB(6, 182, 212), RGB(241, 245, 249), "trita"),
-    Theme(RGB(10, 10, 10), RGB(20, 80, 20), RGB(100, 255, 100), "white"),
-    Theme(RGB(0, 75, 15), RGB(10, 130, 30), RGB(210, 10, 175), "matrix"),
-    Theme(RGB(0, 0, 0), RGB(255, 255, 255), RGB(57, 56, 82), "sans.")
+    Theme(RGB(42, 42, 42), RGB(85, 60, 100), RGB(200, 200, 200), RGB(80, 80, 80), "b/w"),
+    Theme(RGB(50, 5, 85), RGB(253, 0, 219), RGB(100, 30, 90), RGB(200, 70, 220), "pink"),
+    Theme(RGB(15, 23, 42), RGB(6, 182, 212), RGB(241, 245, 249), RGB(110, 110, 110), "white"),
+    Theme(RGB(10, 10, 10), RGB(20, 80, 20), RGB(100, 255, 100), RGB(0, 140, 42), "trita"),
+    Theme(RGB(0, 75, 15), RGB(10, 130, 30), RGB(210, 10, 175), RGB(0, 255, 0), "matrix"),
+    Theme(RGB(0, 0, 0), RGB(255, 255, 255), RGB(57, 56, 82), RGB(200, 194, 226), "sans.")
 ]
 
 def get_bg_list_for_pattern(pattern: List[List[int]], is_sans_pattern: bool) -> List[str]:
@@ -36,5 +37,6 @@ def get_bg_list_for_pattern(pattern: List[List[int]], is_sans_pattern: bool) -> 
         current_theme.wall.to_ansi(),
         current_theme.logo.to_ansi(),
         BG_CORE,
-        current_theme.path.to_ansi()
+        current_theme.path.to_ansi(),
+        current_theme.way.to_ansi()
     ]
